94b30a1d85b1ff10b28139c164c7f044ea889fef
Caio Godoi <caio.godoi@gmail.com>
2018-06-25 09:41:01 -0300

    wrap NewRepoModal form inside a Modal component

--- components/NewRepoModal.js
+++ components/NewRepoModal.js
@@ -13,21 +13,31 @@ export default class NewRepoModal extends Component {
     return (
       <Modal animationType="fade" transparent={true} visible={true}>
         <View style={styles.modalContainer}>
-          <View style={styles.boxContainer} />
-          <Text style={styles.boxTitle}>Adicionar repositório</Text>
-          <TextInput
-            autoFocus
-            autoCapitalize="none"
-            style={styles.boxInput}
-            underlineColorAndroid="rgba(0,0,0,0)"
-            placeholder="organização/repositório"
-          />
-
-          <View style={style.buttonContainer}>
-            <TouchableOpacity
-              style={[styles.button, styles.cancelButton]}
-              onPress={() => {}}
+          <View style={styles.boxContainer}>
+            <Text style={styles.boxTitle}>Adicionar repositório</Text>
+            <TextInput
+              autoFocus
+              autoCapitalize="none"
+              style={styles.boxInput}
+              underlineColorAndroid="rgba(0,0,0,0)"
+              placeholder="organização/repositório"
             />
+
+            <View style={styles.buttonContainer}>
+              <TouchableOpacity
+                style={[styles.button, styles.cancelButton]}
+                onPress={() => {}}
+              >
+                <Text style={styles.buttonText}>Cancelar</Text>
+              </TouchableOpacity>
+
+              <TouchableOpacity
+                style={[styles.button, styles.submitButton]}
+                onPress={() => {}}
+              >
+                <Text style={styles.buttonText}>Adicionar</Text>
+              </TouchableOpacity>
+            </View>
           </View>
         </View>
       </Modal>
@@ -49,6 +59,50 @@ const styles = StyleSheet.create({
     borderRadius: 10,
     alignItems: 'center',
     width: 280,
-    height: 30,
+  },
+
+  boxTitle: {
+    fontWeight: 'bold',
+    fontSize: 16,
+  },
+
+  boxInput: {
+    alignSelf: 'stretch',
+    marginTop: 10,
+    paddingVertical: 0,
+    paddingHorizontal: 20,
+    borderWidth: 1,
+    borderColor: '#DDD',
+    height: 40,
+    borderRadius: 3,
+  },
+
+  buttonContainer: {
+    marginTop: 10,
+    height: 40,
+    flexDirection: 'row',
+  },
+
+  button: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 3,
+  },
+
+  cancelButton: {
+    backgroundColor: '#E25F5F',
+    marginRight: 5,
+  },
+
+  submitButton: {
+    backgroundColor: '#70BD85',
+    marginLeft: 5,
+  },
+
+  buttonText: {
+    fontWeight: 'bold',
+    color: '#FFF',
+    fontSize: 12,
   },
 });
